85533386af2ef8e0bfac5891aa9b1c1ff57d7240
Daniel Alves Freitas <PT3020037@IFSPROOT.PTB>
2024-02-22 20:51:28 -0300

    segundo commit

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
 # A very simple Flask Hello World app for you to get started with...
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return '<p>Hello from Flask!</p><table><tr><td><b>Aluno:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+    
+    return """<h1>Avaliação contínua: Aula 030</h1>
+              <br>
+              <ul>
+                <li><a href=''>Home</a></li>
+                <li><a href=''>Identificação</a></li>
+                <li><a href=''>Contexto da requisição</a></li></ul>
+              <ul>"""
