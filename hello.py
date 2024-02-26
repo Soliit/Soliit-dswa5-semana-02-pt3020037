33dcdcc3dd8be0b3760f8179006e416f3821d5ea
Daniel Alves Freitas <PT3020037@IFSPROOT.PTB>
2024-02-26 20:18:58 -0300

    fourth commit

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 # A very simple Flask Hello World app for you to get started with...
-from flask import Flask
+from flask import Flask, request
+
 
 app = Flask(__name__)
 
@@ -7,12 +8,12 @@ app = Flask(__name__)
 def home():
 
     return """<h1>Avaliação contínua: Aula 030</h1>
-              <br>
               <ul>
                 <li><a href='/'>Home</a></li>
                 <li><a href='/identificacao'>Identificação</a></li>
                 <li><a href='/contexto-da-requisicao'>Contexto da requisição</a></li></ul>
               <ul>"""
+
 
 @app.route('/identificacao')
 def identificacao():
@@ -21,15 +22,17 @@ def identificacao():
               <h2>Aluno: Daniel Alves Freitas</h2>
               <h2>Prontuário: PT3020037</h2>
               <h2>Instituição: IFSP</h2>
-              <p><a href="/">Voltar</a></p>
-            """
+              <p><a href="/">Voltar</a></p>"""
+
 
 @app.route('/contexto-da-requisicao')
 def contexto_da_requisicao():
-    
-    return """<h1>Avaliação contínua: Aula 030</h1>
-              <h2>Seu navegador é: {user_agent}<></h2>
-              <h2>O IP do cumputador remoto é: {ip_address}<></h2>
-              <h2>O host da aplicação é: {app_url}</h2>
-            """
-              
+
+    user_agent = request.headers.get('User-Agent')
+    html = '<h1>Avaliação contínua: Aula 030</h1>'
+    html += f'<h2>Seu navegador é: {user_agent}'
+    html += f'<h2>O IP do cumputador remoto é: {request.remote_addr}</h2>'
+    html += f'<h2>O host da aplicação é: {request.host}</h2>'
+    html += '<p><a href="/">Voltar</a></p>'
+
+    return html
